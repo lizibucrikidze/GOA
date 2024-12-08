82a877802ed8b55c86fd5a2620f1299a44360bf7
lizibucrikidze <lizibucxrikidze@gmail.com>
2024-12-08 22:32:19 +0400

    commit

---
+++ level 18/homework/task5.py
@@ -0,0 +1,45 @@
+# Tasks with a Single Number in the range():
+# Create a range starting from 0 to 5 .
+# Create a range starting from 0 to 10 .
+# Create a range starting from 0 to 7 .
+# Create a range starting from 0 to 3 .
+# Create a range starting from 0 to 12 .
+# Create a range starting from 0 to 9 .
+# Create a range starting from 0 to 15 .
+# Create a range starting from 0 to 20 .
+# reate a range starting from 0 to 25 .
+# Create a range starting from 0 to 30 .
+
+# Tasks with Two Numbers in the range():
+# Create a range from 5 to 10 .
+# Create a range from 3 to 8 .
+# Create a range from 10 to 20 .
+# Create a range from 0 to 5 .
+# Create a range from 7 to 12 .
+# Create a range from 20 to 30 .
+# Create a range from 15 to 25 .
+# Create a range from 5 to 15 .
+# Create a range from 0 to 10 .
+# Create a range from 2 to 8 .
+# Create a range from 10 to 15 .
+# Create a range from 6 to 12 .
+# Create a range from 1 to 6 .
+# Create a range from 8 to 18 .
+# Create a range from 2 to 14 .
+# Create a range from 0 to 8 .
+# Create a range from 12 to 20 .
+# Create a range from 3 to 9 .
+# Create a range from 7 to 13 .
+# Create a range from 4 to 11 .
+
+# Tasks with Three Numbers in the range():
+# Create a range from 0 to 10 with a step of 2.
+# Create a range from 5 to 20 with a step of 3.
+# Create a range from 0 to 15 with a step of 4.
+# Create a range from 10 to 50 with a step of 5.
+# Create a range from 2 to 12 with a step of 2.
+# Create a range from 1 to 10 with a step of 3.
+# Create a range from 0 to 18 with a step of 3.
+# Create a range from 6 to 24 with a step of 4.
+# Create a range from 4 to 16 with a step of 2.
+# Create a range from 8 to 28 with a step of 4.```
---
+++ level 18/homework/task6.py
@@ -0,0 +1,1 @@
+print("I read texts")
